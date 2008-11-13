2074af275cfba0c82adcdc5e31a03f84b007f0f0
Luke Crawford <luke0x@gmail.com>
2008-11-12 20:10:03 -0500

    - 16-step button ui to modify measure info

--- lib/application.rb
+++ lib/application.rb
@@ -1,21 +1,36 @@
+#!/usr/local/bin/macruby
 require 'hotcocoa'
-
-# Replace the following code with your own hotcocoa code
 
 class Application
 
   include HotCocoa
   
+  attr_accessor :measure
+  
   def start
+    self.measure = [false] * 16
+    
     application :name => "Sweet-beats" do |app|
       app.delegate = self
-      window :frame => [100, 100, 500, 500], :title => "Sweet-beats" do |win|
+      window :frame => [100, 100, 800, 100], :title => "Sweet-beats" do |win|
         win << label(:text => "Hello from HotCocoa", :layout => {:start => false})
+
+        win.view = layout_view :mode => :horizontal do |layout|
+          layout.spacing = 0
+          buttons = []
+          16.times do |i|
+            b = button :title => "#{i}", :type => :push_on_push_off, :bezel => :textured_rounded
+            b.on_action { self.measure[i] = b.on?; puts self.measure }
+            buttons << b
+            layout << b
+          end
+        end
+        
         win.will_close { exit }
       end
     end
   end
-  
+
   # file/open
   def on_open(menu)
   end
